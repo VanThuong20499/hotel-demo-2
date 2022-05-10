33857fab7937173dac5f7f29c972976b0c9a6cbd
VanThuong20499 <100428178+VanThuong20499@users.noreply.github.com>
2022-05-10 09:10:02 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -170,7 +170,7 @@
         }
         headerImg.style.backgroundImage = `url('${myArrayImg[count].path}')`;
         if(document.documentElement.clientWidth > 1023){
-            document.querySelector('.header__container-titlxe-btn .active').classList.remove('active');
+            document.querySelector('.header__container-title-btn .active').classList.remove('active');
         }
         headerBtn[count].classList.add('active');
         headerText.textContent = myArrayImg[count].text;
